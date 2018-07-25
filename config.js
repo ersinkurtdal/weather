42b68aead89687a5e0f131fb94445a55eb27b65d
Ersin Kurtdal <ersinkurtdal@gmail.com>
2018-07-25 12:08:04 +0300

    Comments

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000, // Application port
-  googleApiKey: process.env.googleApiKey,
-  weatherSecretKey: process.env.weatherSecretKey
+  googleApiKey: process.env.googleApiKey, // Google Map api key
+  weatherSecretKey: process.env.weatherSecretKey // https://darksky.net/dev api key
 }
